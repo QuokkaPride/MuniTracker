c151540d66288b71f7b37ff7bfa4b4f2bca66ec3
quokka <devonsaliga@gmail.com>
2024-08-21 10:47:01 -0400

    delete works

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ app.post("/submit", (req, res) => {
     
     // Add the new submission to the array
     submissions.push({ issueAmount, issueDate, obligor, id });
-    
+
+
     // Render the page with all submissions
     res.render("index.ejs", { 
         submissions: submissions
@@ -40,9 +41,23 @@ app.post("/submit", (req, res) => {
 
 // delete route 
 app.post('/delete', (req, res) => {
-    let id = req.body.id;
-    submissions = submissions.filter(submission => submission.id !== id);
-    console.log(`Deleted item with id: ${id}`);
+    let id = Number(req.body.id.trim()); // Convert to number after trimming
+    console.table(submissions);
+    console.log(`ID to delete: '${id}' (Type: ${typeof id})`);
+
+    // Find the index of the submission with the given id
+    const index = submissions.findIndex(submission => submission.id === id);
+    console.log(`Position of item with id '${id}' is: ${index}`);
+    
+    if (index !== -1) {
+        submissions = submissions.filter(submission => submission.id !== id); 
+        console.log(`Deleted item with id: '${id}'`);
+    } else {
+        console.log(`Item with id '${id}' not found.`);
+    }
+    
+    console.table(submissions);
+    
     // Render the page with all submissions
     res.render("index.ejs", { 
         submissions: submissions
